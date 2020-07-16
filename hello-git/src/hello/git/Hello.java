e7924ff1cf8175ee7c9d8a29117fedcffd51b079
Alessandro Montaghi <alessandro.montaghi@gmail.com>
2020-07-16 20:58:40 +0200

    add some stuff to master

--- hello-git/src/hello/git/Hello.java
+++ hello-git/src/hello/git/Hello.java
@@ -2,4 +2,8 @@ package hello.git;
 
 public class Hello {
 
+	public void helloDisplay() {
+		System.out.println("I am Git!");
+	}
+	
 }
--- hello-git/src/hello/git/MainHelloGit.java
+++ hello-git/src/hello/git/MainHelloGit.java
@@ -4,6 +4,9 @@ public class MainHelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		
+		Hello helloGit = new Hello();
+		helloGit.helloDisplay();
 
 	}
 
